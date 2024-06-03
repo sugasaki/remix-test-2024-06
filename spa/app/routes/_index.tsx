7cd69e0247ca6d246053c6ee047115a41056decc
sugasaki <sugasaki@gmail.com>
2024-06-03 15:50:37 +0900

    add spa mode

---
+++ spa/app/routes/_index.tsx
@@ -0,0 +1,21 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix SPA" },
+    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (SPA Mode)</h1>
+      <div>
+      <h1>ユーザー画面</h1>
+      <Link to="/admin">管理画面へ</Link>
+    </div>
+    </div>
+  );
+}
---
+++ spa/app/routes/admin.tsx
@@ -0,0 +1,10 @@
+import { Link } from "@remix-run/react";
+
+export default function Admin() {
+  return (
+    <div>
+      <h1>管理画面</h1>
+      <Link to="/">ユーザー画面へ</Link>
+    </div>
+  );
+}
